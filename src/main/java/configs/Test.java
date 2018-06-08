a57ce23fe0cdd4074f9c8b0ea57e2b3a5182de46
Pepe Valdivia <jvaldivia@softweb.pe>
2018-06-08 12:45:16 -0500

    Model de vista

--- src/main/java/configs/Test.java
+++ src/main/java/configs/Test.java
@@ -3,11 +3,13 @@ import java.util.ArrayList;
 import java.util.List;
 import org.json.JSONObject;
 import models.Departamento;
+import models.VWDistritoProvinciaDepartamento;
 
 public class Test {
   public static void main(String args[]){
     Database db = new Database();
     String rpta = "";
+    System.out.println("1 +++++++++++++++++++++++++++++++++++++++++");
     try {
       List<JSONObject> rptaTemp = new ArrayList<JSONObject>();
       db.open();
@@ -29,5 +31,28 @@ public class Test {
       System.out.println(rpta);
       db.close();
     }
+    System.out.println("2 +++++++++++++++++++++++++++++++++++++++++");
+    try {
+      List<JSONObject> rptaTemp = new ArrayList<JSONObject>();
+      db.open();
+      List<VWDistritoProvinciaDepartamento> rptaList = VWDistritoProvinciaDepartamento.findAll();
+      for (VWDistritoProvinciaDepartamento distrito : rptaList) {
+        JSONObject obj = new JSONObject();
+        obj.put("id", distrito.get("id"));
+        obj.put("nombre", distrito.get("nombre"));
+        rptaTemp.add(obj);
+      }
+      rpta = rptaTemp.toString();
+    }catch (Exception e) {
+      String[] error = {"Se ha producido un error en  listar la búsqueda", e.toString()};
+      JSONObject rptaTry = new JSONObject();
+      rptaTry.put("tipo_mensaje", "error");
+      rptaTry.put("mensaje", error);
+      rpta = rptaTry.toString();
+    } finally {
+      System.out.println(rpta);
+      db.close();
+    }
+    System.out.println("3 +++++++++++++++++++++++++++++++++++++++++");
   }
 }
